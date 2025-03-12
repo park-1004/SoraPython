78bc883a456fc81e4def1d23921c03b495fb0403
Sora <qkrwhddnr22@gmail.com>
2025-03-12 18:56:31 +0900

    'ch06'

---
+++ ch06/handle_with_try.py
@@ -0,0 +1,9 @@
+# try except 구문으로 예외를 처리합니다.
+try:
+    number_input_a = int(input("정수 입력> "))
+    # 출력합니다.
+    print("원의 반지름:", number_input_a)
+    print("원의 둘레:", 2 * 3.14 * number_input_a)
+    print("원의 넓이:", 3.14 * number_input_a * number_input_a)
+except:
+    print("무언가 잘못되었습니다.")
